27b8432de5bf152c6ddcfa7d66840661d88ebad4
Rafael Chies <rafachies@gmail.com>
2012-05-06 20:32:34 -0300

    adicionando exemplo de servico REST

--- src/main/java/br/net/tonalista/rest/ToNaListaApplication.java
+++ src/main/java/br/net/tonalista/rest/ToNaListaApplication.java
@@ -1,9 +1,11 @@
 package br.net.tonalista.rest;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/rest")
+@ApplicationScoped
 public class ToNaListaApplication extends Application{
 
 }
